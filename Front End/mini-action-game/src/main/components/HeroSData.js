f2938cce234a728dd43a5153578dad1b9d354998
Qephetzial <prekopadominik@gmail.com>
2023-06-08 09:22:35 +0200

    refactor -> removed an unused part from heroes data display and add the heroes value

--- Front End/mini-action-game/src/main/components/HeroSData.js
+++ Front End/mini-action-game/src/main/components/HeroSData.js
@@ -5,19 +5,19 @@ function HeroSData({hero}) {
         <div className='place'>
             <div className='data inLine'>
                 <h5>Class:&nbsp;</h5>
+                <h5>Price:&nbsp;</h5>
                 <h5>HitPoint:&nbsp;</h5>
                 <h5>Damage:&nbsp;</h5>
                 <h5>Defence:&nbsp;</h5>
                 <h5>Movement Speed:&nbsp;</h5>
-                <h5>UltimatePower:&nbsp;</h5>
             </div>
             <div className='data2 inLine'>
                 <h5>{hero.name}</h5>
+                <h5>{hero.value}</h5>
                 <h5>{hero.health}</h5>
                 <h5>{hero.strength}</h5>
                 <h5>{hero.defence}</h5>
                 <h5>{hero.movementSpeed}</h5>
-                <h5>None</h5>
             </div>
         </div>
     )
