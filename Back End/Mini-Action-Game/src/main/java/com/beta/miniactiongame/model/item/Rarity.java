b3743bfb5a30fa1edede4b5d3f8de68f47c6f2d2
Qephetzial <prekopadominik@gmail.com>
2023-06-08 10:53:40 +0200

    refactor -> doesn't need the price of the item in the back-end

--- Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/model/item/Rarity.java
+++ Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/model/item/Rarity.java
@@ -1,21 +1,9 @@
 package com.beta.miniactiongame.model.item;
 
 public enum Rarity {
-    COMMON(10),
-    UNCOMMON(30),
-    RARE(90),
-    EPIC(270),
-    LEGENDARY(810);
-
-    private final int value;
-
-
-    Rarity(int price){
-        value = price;
-    }
-
-
-    public int getValue() {
-        return value;
-    }
+    COMMON,
+    UNCOMMON,
+    RARE,
+    EPIC,
+    LEGENDARY;
 }
