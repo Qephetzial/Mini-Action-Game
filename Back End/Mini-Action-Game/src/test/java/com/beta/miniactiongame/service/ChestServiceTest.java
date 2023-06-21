c6a3100e5ef46d6fccf3501efa209f962598334e
Qephetzial <prekopadominik@gmail.com>
2023-06-21 11:41:00 +0200

    add -> junit test for ChestService

---
+++ Back End/Mini-Action-Game/src/test/java/com/beta/miniactiongame/service/ChestServiceTest.java
@@ -0,0 +1,67 @@
+package com.beta.miniactiongame.service;
+
+import com.beta.miniactiongame.factory.ArmorFactory;
+import com.beta.miniactiongame.factory.WeaponFactory;
+import com.beta.miniactiongame.model.item.Items;
+import com.beta.miniactiongame.model.item.Rarity;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChestServiceTest {
+
+    private final ChestService chestService = new ChestService(new ArmorFactory(), new WeaponFactory());
+
+    @Test
+    void openChest() {
+        List<Items> item = chestService.openChest(800, 950, 990, 1000, false);
+        assertNotNull(item);
+    }
+
+    @Test
+    void getCommonItem() {
+        List<Items> item = chestService.openChest(1000,0,0,0,false);
+        assertEquals(Rarity.COMMON, item.get(0).getRarity());
+    }
+
+    @Test
+    void getUnCommonItem() {
+        List<Items> item = chestService.openChest(0,1000,0,0,false);
+        assertEquals(Rarity.UNCOMMON, item.get(0).getRarity());
+    }
+
+    @Test
+    void getRareItem() {
+        List<Items> item = chestService.openChest(0,0,1000,0,false);
+        assertEquals(Rarity.RARE, item.get(0).getRarity());
+    }
+
+    @Test
+    void getEpicItem() {
+        List<Items> item = chestService.openChest(0,0,0,1000,false);
+        assertEquals(Rarity.EPIC, item.get(0).getRarity());
+    }
+
+    @Test
+    void getLegendaryItem() {
+        List<Items> item = chestService.openChest(0,0,0,0,false);
+        assertEquals(Rarity.LEGENDARY, item.get(0).getRarity());
+    }
+
+    @Test
+    void getBonusLoot() {
+        boolean needBonusLoot = true;
+        int counter = 0;
+        List<Items> items;
+        while (needBonusLoot && counter < 100000) {
+            items = chestService.openChest(200, 400, 600, 800, true);
+            if (items.size() == 2) {
+                needBonusLoot = false;
+            }
+            counter ++;
+        }
+        assertFalse(needBonusLoot);
+    }
+}
