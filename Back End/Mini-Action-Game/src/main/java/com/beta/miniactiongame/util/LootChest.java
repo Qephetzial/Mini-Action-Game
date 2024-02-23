741036a95c1d90c66e8ce150cb3919491b073aec
dominik.prekopa <dominik.prekopa@nixs.com>
2024-02-23 12:08:02 +0100

    refactor -> name field added

--- Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/util/LootChest.java
+++ Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/util/LootChest.java
@@ -5,12 +5,12 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public final class LootChest {
 
-    private final Chest ironChest = new Chest(800, 950, 990, 1000, false);
-    private final Chest bronzeChest = new Chest(703, 905, 977, 999, false);
-    private final Chest silverChest = new Chest(600, 850, 950, 995, false);
-    private final Chest goldenChest = new Chest(500, 750, 900, 990, true);
+    private final Chest ironChest = new Chest("ironChest", 800, 950, 990, 1000, false);
+    private final Chest bronzeChest = new Chest("bronzeChest", 703, 905, 977, 999, false);
+    private final Chest silverChest = new Chest("silverChest", 600, 850, 950, 995, false);
+    private final Chest goldenChest = new Chest("goldenChest", 500, 750, 900, 990, true);
 
-    public record Chest(int commonLootChance, int unCommonLootChance, int rareLootChance, int epicLootChance, boolean chanceForBonusLoot) {}
+    public record Chest(String name, int commonLootChance, int unCommonLootChance, int rareLootChance, int epicLootChance, boolean chanceForBonusLoot) {}
 
     public static Chest getChest(ChestType chestType) {
         return switch (chestType) {
