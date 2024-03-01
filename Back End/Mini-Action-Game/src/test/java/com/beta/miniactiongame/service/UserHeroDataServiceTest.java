ff60761d885f2d77f79d3387c80bd52cc38c93b1
dominik.prekopa <dominik.prekopa@nixs.com>
2024-03-01 16:42:02 +0100

    add -> test for changing items

--- Back End/Mini-Action-Game/src/test/java/com/beta/miniactiongame/service/UserHeroDataServiceTest.java
+++ Back End/Mini-Action-Game/src/test/java/com/beta/miniactiongame/service/UserHeroDataServiceTest.java
@@ -36,13 +36,19 @@ class UserHeroDataServiceTest {
     private final Hero ranger = new Hero();
     private final Hero mage = new Hero();
     private final Hero demon = new Hero();
-    private final Weapon weapon = new Weapon();
-    private final Armor armor = new Armor();
+    private final Weapon weaponOne = new Weapon();
+    private final Weapon weaponTwo = new Weapon();
+    private final Armor armorOne = new Armor();
+    private final Armor armorTwo= new Armor();
     List<UserHeroData> allData;
     private static final UUID FIGHTER_ID = UUID.randomUUID();
     private static final UUID RANGER_ID = UUID.randomUUID();
     private static final UUID MAGE_ID = UUID.randomUUID();
     private static final UUID DEMON_ID = UUID.randomUUID();
+    private static final UUID WEAPON_ONE_ID = UUID.randomUUID();
+    private static final UUID WEAPON_TWO_ID = UUID.randomUUID();
+    private static final UUID ARMOR_ONE_ID = UUID.randomUUID();
+    private static final UUID ARMOR_TWO_ID = UUID.randomUUID();
 
     @BeforeEach
     void setup() {
@@ -51,16 +57,20 @@ class UserHeroDataServiceTest {
         ranger.setId(RANGER_ID);
         mage.setId(MAGE_ID);
         demon.setId(DEMON_ID);
+        weaponOne.setId(WEAPON_ONE_ID);
+        weaponTwo.setId(WEAPON_TWO_ID);
+        armorOne.setId(ARMOR_ONE_ID);
+        armorTwo.setId(ARMOR_TWO_ID);
         when(heroFactory.getFighter()).thenReturn(fighter);
         when(heroFactory.getRanger()).thenReturn(ranger);
         when(heroFactory.getMage()).thenReturn(mage);
         when(heroFactory.getDemon()).thenReturn(demon);
-        when(armorFactory.getCommonArmorOne()).thenReturn(armor);
-        when(armorFactory.getRareArmorOne()).thenReturn(armor);
-        when(weaponFactory.getCommonSwordOne()).thenReturn(weapon);
-        when(weaponFactory.getCommonBowOne()).thenReturn(weapon);
-        when(weaponFactory.getCommonStaffOne()).thenReturn(weapon);
-        when(weaponFactory.getRareStaffOne()).thenReturn(weapon);
+        when(armorFactory.getCommonArmorOne()).thenReturn(armorOne);
+        when(armorFactory.getRareArmorOne()).thenReturn(armorOne);
+        when(weaponFactory.getCommonSwordOne()).thenReturn(weaponOne);
+        when(weaponFactory.getCommonBowOne()).thenReturn(weaponOne);
+        when(weaponFactory.getCommonStaffOne()).thenReturn(weaponOne);
+        when(weaponFactory.getRareStaffOne()).thenReturn(weaponOne);
         allData = userHeroDataService.getUserHeroData();
     }
 
@@ -121,5 +131,23 @@ class UserHeroDataServiceTest {
                 assertFalse(data.isSelected());
             }
         }
+    }
+
+    @Test
+    void changeWeapon() {
+        UserHeroData userHeroData = allData.get(0);
+        assertEquals(WEAPON_ONE_ID, userHeroData.getWeapon().getId());
+        Weapon weapon = userHeroDataService.changeWeapon(userHeroData, weaponTwo);
+        assertEquals(WEAPON_TWO_ID, userHeroData.getWeapon().getId());
+        assertEquals(WEAPON_ONE_ID, weapon.getId());
+    }
+
+    @Test
+    void changeArmor() {
+        UserHeroData userHeroData = allData.get(0);
+        assertEquals(ARMOR_ONE_ID, userHeroData.getArmor().getId());
+        Armor armor = userHeroDataService.changeArmor(userHeroData, armorTwo);
+        assertEquals(ARMOR_TWO_ID, userHeroData.getArmor().getId());
+        assertEquals(ARMOR_ONE_ID, armor.getId());
     }
 }
