cf201ac60d0ce8b708d62d8afe1836db9f5bb002
dominik.prekopa <dominik.prekopa@nixs.com>
2024-03-01 09:59:04 +0100

    refactor -> delete unused imports

--- Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/auth/RegisterRequest.java
+++ Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/auth/RegisterRequest.java
@@ -2,13 +2,13 @@ package com.beta.miniactiongame.auth;
 
 import com.beta.miniactiongame.model.Role;
 import com.beta.miniactiongame.model.UserHeroData;
-import com.beta.miniactiongame.model.creature.Hero;
 import com.beta.miniactiongame.model.item.Armor;
 import com.beta.miniactiongame.model.item.Weapon;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
