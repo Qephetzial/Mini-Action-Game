159d5874c40309d5fe53914036be7441621dcd3f
dominik.prekopa <dominik.prekopa@nixs.com>
2024-03-01 17:08:58 +0100

    refactor -> remove unused imports

--- Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/repository/hero/HeroRepository.java
+++ Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/repository/hero/HeroRepository.java
@@ -1,6 +1,5 @@
 package com.beta.miniactiongame.repository.hero;
 
-import com.beta.miniactiongame.model.creature.CreatureType;
 import com.beta.miniactiongame.model.creature.Hero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
