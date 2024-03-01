f2f92b99e8cd4535fb0ecb0cebeee72bbcc72514
dominik.prekopa <dominik.prekopa@nixs.com>
2024-03-01 17:09:27 +0100

    add -> HeroDataWrapperNotFound runtime exception

---
+++ Back End/Mini-Action-Game/src/main/java/com/beta/miniactiongame/exceptions/HeroDataWrapperNotFound.java
@@ -0,0 +1,7 @@
+package com.beta.miniactiongame.exceptions;
+
+public class HeroDataWrapperNotFound extends RuntimeException{
+    public HeroDataWrapperNotFound(String message) {
+        super(message);
+    }
+}
